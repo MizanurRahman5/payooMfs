f068ec52cc2dd150ff5034194cabfb4328fb3c66
MistyIslam5 <mdmisty5@gmail.com>
2024-09-18 23:11:30 +0600

    login form and fucntion added

--- js/login.js
+++ js/login.js
@@ -5,7 +5,18 @@ document.getElementById('login-btn').addEventListener('click', function(event){
     event.preventDefault();
 
     // step 2 get phone Number
-    const phoneNumber = document.getElementById('phone-number');
-    console.log(phoneNumber.value)
+    const phoneNumber = document.getElementById('phone-number').value;
+    const pinNumber = document.getElementById('pin-number').value;
+    // console.log(phoneNumber.value)
+    // console.log(phoneNumber, pinNumber)
+    phoneNumber.value = ' ';
+
+    if (phoneNumber === '5' && pinNumber === '1234'){
+        // console.log('you Are logged')
+        window.location.href = '/home.html';
+    }else{
+        alert('your pin or Number encorrect');
+    }
+
 
 })
